61e6daeb4b16a9eb2f36ee44cb89eea9b164b5ca
Ke Zhang <ke.zhang@austcommunitymedia.com.au>
2025-11-11 09:38:44 +1100

    Implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+  const fetchAllPromises = urls.map(async (url) => {
+    const response = await httpGet(url);
+    const result = JSON.parse(response.body);
+    if (response.status === 200) return { "Arnie Quote": result.message };
+    return { FAILURE: result.message };
+  });
+
+  const results = await Promise.allSettled(fetchAllPromises);
+  return results.map((result) => result.value);
 };
 
 module.exports = {
